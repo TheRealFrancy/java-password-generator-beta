4d666eda3aacc941f112103c10117882d6e77b12
francesco <francyespo77@gmail.com>
2023-01-11 21:04:28 +0100

    first commit

---
+++ src/pswgen/Main.java
@@ -0,0 +1,45 @@
+import  java.awt.*;
+public class Main {
+    public static void main(String[] args) {
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+
+        String uniqueidchar="abcdefhilmnpqrstukwz12345";
+        char[] resultpsw = new char[10];
+        for (int i = 0; i < 10; i++) {
+         int n =  1+ (int) (Math.random()*10);
+         resultpsw[i] =uniqueidchar.charAt(n);
+        }
+        String pswfinal=new String(resultpsw);
+
+        System.out.println(pswfinal);
+         */
+
+    }// end main
+}// end class
---
+++ src/pswgen/frameview.java
@@ -0,0 +1,6 @@
+package pswgen;
+
+import javax.swing.*;
+
+public class frame extends JFrame {
+}
