f37ef0a3f9b1e6d50fe0b7595f33dad2c3c26f76
TheRealFrancy <38110096+TheRealFrancy@users.noreply.github.com>
2023-01-11 21:29:34 +0100

    Update frameview.java

--- frameview.java
+++ frameview.java
@@ -1,12 +1,5 @@
 import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 public class frameview extends JFrame {
     private final JButton bottone = new JButton("genera");
@@ -15,42 +8,40 @@ public class frameview extends JFrame {
 
     public frameview() {
         super("psw gen");
-        this.setLayout(new BorderLayout());
-        this.add(this.textField, "First");
-        this.add(this.bottone, "Last");
-        this.add(this.textArea, "Center");
-        this.textField.setToolTipText("inserisci lunghezza password ");
-        this.textArea.setEditable(false);
-        this.bottone.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String lenghtppsw = frameview.this.textField.getText();
+        setLayout(new BorderLayout());
+        add(textField, BorderLayout.PAGE_START);
+        add(bottone, BorderLayout.PAGE_END);
+        add(textArea, BorderLayout.CENTER);
+        textField.setToolTipText("inserisci lunghezza password ");
+       textArea.setEditable(false);
+      bottone.addActionListener(e -> {
+          String lenghtppsw = textField.getText();
 
-                try {
-                    String uniqueidchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$&%";
-                    char[] resultpsw = new char[Integer.parseInt(lenghtppsw)];
+          try {
+              String uniqueidchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$&%";
+              char[] resultpsw = new char[Integer.parseInt(lenghtppsw)];
 
-                    for(int i = 0; i < Integer.parseInt(lenghtppsw); ++i) {
-                        int n = 1 + (int)(Math.random() * (double)Integer.parseInt(lenghtppsw));
-                        resultpsw[i] = uniqueidchar.charAt(n);
-                    }
+              for(int i = 0; i < Integer.parseInt(lenghtppsw); ++i) {
+                  int n = 1 + (int)(Math.random() * (double)Integer.parseInt(lenghtppsw));
+                  resultpsw[i] = uniqueidchar.charAt(n);
+              }
 
-                    String pswfinal = new String(resultpsw);
-                    frameview.this.textArea.append(pswfinal + "\n");
-                } catch (StringIndexOutOfBoundsException var7) {
-                    JOptionPane.showMessageDialog((Component)null, "Non puoi inserire piu di 64 carrateri.", "Attenzione", 2);
-                    var7.printStackTrace();
-                } catch (NumberFormatException var8) {
-                    JOptionPane.showMessageDialog((Component)null, "Errore non e un numero intero", "errore", 0);
-                    var8.printStackTrace();
-                    System.exit(1);
-                }
+              String pswfinal = new String(resultpsw);
+              textArea.append(pswfinal + "\n");
+          } catch (StringIndexOutOfBoundsException var7) {
+              JOptionPane.showMessageDialog(null, "Non puoi inserire piu di 64 carrateri.", "Attenzione", JOptionPane.WARNING_MESSAGE);
+              var7.printStackTrace();
+          } catch (NumberFormatException var8) {
+              JOptionPane.showMessageDialog(null, "Errore non e un numero intero", "errore", JOptionPane.ERROR_MESSAGE);
+              var8.printStackTrace();
+              System.exit(1);
+          }
 
-            }
-        });
-        this.setResizable(false);
-        this.setLocation(500, 400);
-        this.setSize(800, 300);
-        this.setVisible(true);
-        this.setDefaultCloseOperation(3);
+      });
+        setResizable(false);
+        setLocation(500, 400);
+        setSize(800, 300);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
