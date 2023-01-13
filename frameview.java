1eb31856980f3f98aeb5911865630d2a1f797f48
TheRealFrancy <38110096+TheRealFrancy@users.noreply.github.com>
2023-01-13 18:04:22 +0100

    Update frameview.java

--- frameview.java
+++ frameview.java
@@ -1,14 +1,20 @@
+package pswgen;
+
 import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class frameview extends JFrame {
-    private final JButton bottone = new JButton("genera");
-    private final JTextArea textArea = new JTextArea();
-    private final JTextField textField = new JTextField();
+    private JButton bottone;
+    private JTextArea textArea;
+    private JTextField textField;
 
     public frameview() {
         super("psw gen");
         setLayout(new BorderLayout());
+        textField = new JTextField();
+        bottone = new JButton("genera");
+        textArea = new JTextArea();
         add(textField, BorderLayout.PAGE_START);
         add(bottone, BorderLayout.PAGE_END);
         add(textArea, BorderLayout.CENTER);
@@ -16,34 +22,25 @@ public class frameview extends JFrame {
         add(scrollbar);
         textField.setToolTipText("inserisci lunghezza password");
         textArea.setEditable(false);
-        bottone.addActionListener(e -> {
-          String lenghtppsw = textField.getText();
-
-          try {
-              String uniqueidchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$&%";
-              char[] resultpsw = new char[Integer.parseInt(lenghtppsw)];
-
-              for(int i = 0; i < Integer.parseInt(lenghtppsw); ++i) {
-                  int n = 1 + (int)(Math.random()*Integer.parseInt(lenghtppsw));
-                  resultpsw[i] = uniqueidchar.charAt(n);
-              }
-
-              String pswfinal = new String(resultpsw);
-              textArea.append(pswfinal + "\n");
-          } catch (StringIndexOutOfBoundsException e1) {
-              JOptionPane.showMessageDialog(null, "Non puoi inserire piu di 64 carrateri.", "Attenzione", JOptionPane.WARNING_MESSAGE);
-              e1.printStackTrace();
-          } catch (NumberFormatException e1) {
-              JOptionPane.showMessageDialog(null, "Errore non e un numero intero", "errore", JOptionPane.ERROR_MESSAGE);
-              e1.printStackTrace();
-              System.exit(1);
-          }
-
-      });
         setResizable(false);
         setLocation(500, 400);
         setSize(800, 300);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+    public void setButtonActionListener(ActionListener listener){
+        bottone.addActionListener(listener);
+    }
+    public String getLenghtppsw(){
+        return textField.getText();
+    }
+    public void appendTextArea(String text){
+        textArea.append(text + "\n");
+    }
+    public void showMessageDialog(String message,String title,int messageType){
+        JOptionPane.showMessageDialog(null, message, title, messageType);
+    }
+    public void exitOnError(){
+        System.exit(1);
     }
 }
