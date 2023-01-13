142ca286599abb0fd5d5d0532e8accb75953fdf4
TheRealFrancy <38110096+TheRealFrancy@users.noreply.github.com>
2023-01-13 18:04:42 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
+package pswgen;
+
 public class Main {
 
     public static void main(String[] args) {
-        new frameview();
+        frameview frameView = new frameview();
+        Model model = new Model();
+        new Controller(frameView, model);
+
     }
+
 }
